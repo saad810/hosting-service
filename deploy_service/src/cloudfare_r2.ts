b8a757970bf66189472bb8872e0995c97ef70643
saad810 <saadameen810@gmail.com>
2025-08-08 23:17:01 +0500

    deploy service update

---
+++ deploy_service/src/cloudfare_r2.ts
@@ -0,0 +1,53 @@
+import { config } from "dotenv";
+import { S3 } from "aws-sdk";
+import fs,{ readFileSync } from "fs";
+import path from "path";
+config();
+
+const Id = process.env.R2_ACCESS_KEY || "";
+const Secret = process.env.R2_SECRET_KEY || "";
+const R2_URL = process.env.R2_URL || "";
+
+const BucketName = "hosting-service";
+const s3 = new S3({
+    accessKeyId: Id,
+    secretAccessKey: Secret,
+    endpoint: R2_URL,
+})
+
+export const downloadS3Folder = async (dir: string) => {
+    if (fs.existsSync(dir)) {
+        console.log(`Directory ${dir} already exists, skipping download.`);
+        return;
+    }
+    console.log(`Downloading files from S3 to ${dir}`);
+    const allFiles = await s3.listObjectsV2({
+        Bucket: BucketName,
+        Prefix: dir
+    }).promise();
+
+    console.log(`Found ${allFiles?.Contents?.length} files in S3`);
+    const allPromises = allFiles.Contents?.map(async ({ Key }) => {
+        return new Promise(async (resolve) => {
+            if (!Key) {
+                resolve("");
+                return;
+            }
+            const finalOutputPath = path.join(__dirname, Key);
+            const outputFile = fs.createWriteStream(finalOutputPath);
+            const dirName = path.dirname(finalOutputPath);
+            if (!fs.existsSync(dirName)) {
+                fs.mkdirSync(dirName, { recursive: true });
+            }
+            s3.getObject({
+                Bucket: BucketName,
+                Key
+            }).createReadStream().pipe(outputFile).on("finish", () => {
+                resolve("");
+            })
+        })
+    }) || []
+    console.log("awaiting");
+
+    await Promise.all(allPromises?.filter(x => x !== undefined));
+}
---
+++ deploy_service/src/index.ts
@@ -0,0 +1,26 @@
+import Redis, { Command } from 'ioredis'
+import { config } from "dotenv";
+import { downloadS3Folder } from './cloudfare_r2';
+
+config();
+
+console.log(process.env.REDIS_URL ? "Redis URL is set" : "Redis URL is not set");
+const subscriber = new Redis(process.env.REDIS_URL || '');
+
+async function main() {
+    while(1){
+        const res = await subscriber.brpop(
+            "builder_queue",0
+        );
+        console.log(`Received message: ${typeof(res)}\t ${res},\t length: ${res?.length}`);
+        // @ts-ignore
+        const id = res[1];
+        console.log(`Processing id: ${id}`);
+        const dir = `./output/${id}/`;
+        await downloadS3Folder(dir);
+    }
+}
+
+main().catch((err) => {
+    console.error("Error in main:", err);
+});

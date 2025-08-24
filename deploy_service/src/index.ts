e85b68d29b394cdad4ee42837f2f4652802106e7
saad810 <saadameen810@gmail.com>
2025-08-25 03:19:42 +0500

    messages

--- deploy_service/src/index.ts
+++ deploy_service/src/index.ts
@@ -8,13 +8,14 @@ config();
 
 console.log(process.env.REDIS_URL ? "Redis URL is set" : "Redis URL is not set");
 const subscriber = new Redis(process.env.REDIS_URL || '');
+const publisher = new Redis(process.env.REDIS_URL || '');
 
 async function main() {
-    while(1){
+    while (1) {
         const res = await subscriber.brpop(
-            "builder_queue",0
+            "builder_queue", 0
         );
-      
+
         // @ts-ignore
         const id = res[1];
         // console.log(`Processing id: ${id}`);
@@ -26,6 +27,8 @@ async function main() {
         console.log("uploading dist")
         await uploadDistFolder(id);
         console.log("finished processing id:", id);
+        publisher.hset("status", id, "deployed");
+        ;
     }
 }
 
